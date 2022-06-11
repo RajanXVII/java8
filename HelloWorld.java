d8be85147c24b7d87ee5a8e3e126840cfec440f1
Rajan Sonvane <44581384+RajanXVII@users.noreply.github.com>
2022-06-11 12:12:20 +0530

    Last third salary java 8

---
+++ HelloWorld.java
@@ -0,0 +1,31 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class Employee{
+    int id;
+    int salary;
+    
+    
+    public Employee(int id,int name){
+        this.id=id;
+        this.salary=name;
+    }
+    public int getSalary(){
+        return this.salary;
+    }
+}
+class HelloWorld {
+    public static void main(String[] args) {
+        
+        List<Employee> i=new ArrayList();
+        i.add(new Employee(1,15000));
+        i.add(new Employee(1,4500));
+        i.add(new Employee(1,7500));
+        i.add(new Employee(1,1700));
+        i.add(new Employee(1,20000));
+        i.add(new Employee(1,5500));
+        //Last third salary
+        Optional<Integer> ne=i.stream().map(e->e.getSalary()).sorted(Comparator.reverseOrder()).skip(2).findFirst();
+        System.out.println(ne.get());
+    }
+}
